2c6d30a4e2c65d9f1a1059e45757cbc07d9739ec
ndjndj <teq.ndj@gmail.com>
2021-12-19 22:33:55 +0900

    test

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -102,6 +102,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
       ),
