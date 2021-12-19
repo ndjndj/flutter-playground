1da4c3917a07d925215eae236361ad71a6df40d0
ndjndj <teq.ndj@gmail.com>
2021-12-20 00:06:11 +0900

    fix

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  // 最初に表示するWidget
   runApp(MyTodoApp());
 }
 
 class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // アプリ名
       title: 'My Todo App',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        // テーマカラー
+        primarySwatch: Colors.blue,
       ),
+      // リスト一覧画面を表示
       home: TodoListPage(),
     );
   }
 }
 
+// リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
+  const TodoListPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('リスト一覧画面'),
-      ),
+
     );
   }
 }
