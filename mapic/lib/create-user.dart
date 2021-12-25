d1d48eb88549b5ffea361b74c41fc5c53629b034
ndjndj <teq.ndj@gmail.com>
2021-12-26 01:50:09 +0900

    createUser

---
+++ mapic/lib/create-user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+
+void main() {
+  runApp(CreateUser());
+}
+
+class CreateUser extends StatefulWidget {
+  @override
+  _CreateUser createState() => _CreateUser();
+}
