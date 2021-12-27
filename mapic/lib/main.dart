9eda2a7c8bbe23a24b00fccef8bfffb5618ccdbf
ndjndj <teq.ndj@gmail.com>
2021-12-28 02:16:13 +0900

    navbar

--- mapic/lib/main.dart
+++ mapic/lib/main.dart
@@ -40,6 +40,23 @@ class _TopState extends State<Top> {
     Top()
   ];
 
+  List<BottomNavigationBarItem> bottomNavBar() {
+    return [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.add_circle_outline),
+        label: 'Add',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.list),
+        label: 'list'
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.miscellaneous_services),
+        label: 'config'
+      ),
+    ]
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
