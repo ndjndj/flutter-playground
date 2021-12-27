b6ccbf9027c9124ac0c293651d5d2390ab3fcae6
ndjndj <teq.ndj@gmail.com>
2021-12-28 02:03:00 +0900

    fix

--- mapic/lib/main.dart
+++ mapic/lib/main.dart
@@ -49,21 +49,6 @@ class _TopState extends State<Top> {
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newText = await Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              return AddMapicPage();
-            })
-          );
-          if (newText != null) {
-            setState(() {
-              addressList.add(newText);
-            });
-          }
-        },
-        child: Icon(Icons.add)
-      ),
     );
   }
 }
