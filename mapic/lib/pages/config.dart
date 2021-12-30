57c330b9dd703377c39667e2416f33676e01f680
ndjndj <teq.ndj@gmail.com>
2021-12-31 03:42:45 +0900

    config.dart

---
+++ mapic/lib/pages/config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+
+class Config extends StatefulWidget {
+  @override
+  _ConfigState createState() => _ConfigState();
+}
+
+class _ConfigState extends State<Config> {
+  List<String> _configList = [
+    'public setting',
+    
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Config'),
+      ),
+      body: ListView.builder(
+        itemCount: _configList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(_configList[index])
+            )
+          )
+        }
+      )
+    );
+  }
+}
