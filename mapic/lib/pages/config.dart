dac5b0e60a87c14e7c6f64ce8a5659e9243f2fa9
ndjndj <teq.ndj@gmail.com>
2022-01-02 02:07:50 +0900

    import dropdown

--- mapic/lib/pages/config.dart
+++ mapic/lib/pages/config.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:io';
+import '../components/dropdown.dart';
 
 class Config extends StatefulWidget {
   @override
