88ffb1e2f2c22934f513f0defe8ed1b98cdb02b4
ndjndj <teq.ndj@gmail.com>
2022-01-10 21:39:59 +0900

    fix

---
+++ mapic/lib/db/database_helper.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static final _databaseName = 'mapic.db';
+  static final _databaseVersion = 1;
+
+  static final table = 'test_table';
+
+  static final columnId = '_id';
+  static final columnName = 'name';
+  static final columnAge = 'age';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+
+    _database = await _initDatabase();
+    return _database;
+  }
+
+
+}
