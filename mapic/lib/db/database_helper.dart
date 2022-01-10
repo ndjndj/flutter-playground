a448836e08b4483d26af29cc2034bd8196c2815a
ndjndj <teq.ndj@gmail.com>
2022-01-11 01:31:32 +0900

    FIX

--- mapic/lib/db/database_helper.dart
+++ mapic/lib/db/database_helper.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 
-
+class SQLHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute(
+      '''CREATE TABLE items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      '''
+    );
+  }
+}
