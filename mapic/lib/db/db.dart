3333b043132d90f221fc2b5f6c69164bd335cb56
ndjndj <teq.ndj@gmail.com>
2022-01-12 00:36:11 +0900

    db.dart

--- mapic/lib/db/db.dart
+++ mapic/lib/db/db.dart
@@ -2,3 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart' as sql;
+
+class DB {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute(
+      '''CREATE TABLE snapshots(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        place TEXT,
+        description TEXT,
+        latitude REAL,
+        longtitude REAL,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      '''
+    );
+  }
+}
