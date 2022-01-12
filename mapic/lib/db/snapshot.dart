7a92d9cc5c6ae01c2b39cb585147333e185899f6
ndjndj <teq.ndj@gmail.com>
2022-01-13 01:07:56 +0900

    create

--- mapic/lib/db/snapshot.dart
+++ mapic/lib/db/snapshot.dart
@@ -2,7 +2,24 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart' as sql;
-
+import './db.dart' as db;
 class SnapShot {
-  
+  static Future<int> createSnapshot(
+    String place,
+    String? description,
+    double? latitude,
+    double? longtitude
+  ) async {
+    final database = await db.DB.db();
+
+    final data = {
+      'place': place,
+      'description': description,
+      'latitude': latitude,
+      'longtitud': longtitude
+    };
+
+    final id = await database.insert('snapshots', data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
 }
