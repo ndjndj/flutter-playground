66a076851ccb9ccae4a00071ca25078657c643a6
ndjndj <teq.ndj@gmail.com>
2022-01-17 00:22:47 +0900

    fix

--- mapic/lib/pages/map-sample.dart
+++ mapic/lib/pages/map-sample.dart
@@ -3,16 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
-    );
-  }
-}
-
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
