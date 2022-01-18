1af5bafbeef43a324a7f0f5ad8185849e78be6ca
ndjndj <teq.ndj@gmail.com>
2022-01-19 01:59:17 +0900

    fix

--- mapic/lib/pages/map-sample.dart
+++ mapic/lib/pages/map-sample.dart
@@ -26,7 +26,7 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
