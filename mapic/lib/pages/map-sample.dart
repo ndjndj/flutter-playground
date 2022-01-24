a98b40d97d587147ab58803f27d5d40dbd4c5325
ndjndj <teq.ndj@gmail.com>
2022-01-25 00:03:06 +0900

    fix

--- mapic/lib/pages/map-sample.dart
+++ mapic/lib/pages/map-sample.dart
@@ -21,13 +21,6 @@ class MapSampleState extends State<MapSample> {
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
