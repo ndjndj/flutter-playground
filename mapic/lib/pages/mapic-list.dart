c7f18c29bea3be60744eced27eb8ae7901b96f0c
ndjndj <teq.ndj@gmail.com>
2022-02-06 18:16:03 +0900

    fix

--- mapic/lib/pages/mapic-list.dart
+++ mapic/lib/pages/mapic-list.dart
@@ -32,6 +32,20 @@ class _MapicList extends State<MapicList> {
       body: Stack(
         children: <Widget>[
           Positioned(
+            top: 450.0,
+            left: 0.0,
+            right: 0.0,
+            child: Card(
+              margin: EdgeInsets.zero,
+              elevation: 3.0,
+              color: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(30.0))
+              ),
+              child: Container(width: double.infinity, height: 200.0)
+            ),
+          ),
+          Positioned(
             top: 300.0,
             left: 0.0,
             right: 0.0,
