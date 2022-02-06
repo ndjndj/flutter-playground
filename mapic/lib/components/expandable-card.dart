5621edc3e78ef4a3d08e9beb35afa7946130ebe2
ndjndj <teq.ndj@gmail.com>
2022-02-06 20:32:09 +0900

    fix

--- mapic/lib/components/expandable-card.dart
+++ mapic/lib/components/expandable-card.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
 
 
+class ExpandableCard extends StatefulWidget {
+  @override
+  _ExpandableCard createState() => _ExpandableCard();
+}
 
+class _ExpandableCard extends State<ExpandableCard> {
+  
+}
