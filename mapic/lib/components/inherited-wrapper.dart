ef4fa0f2e2f94991c0b6f50c92d96cb1e40db352
ndjndj <teq.ndj@gmail.com>
2022-02-09 21:16:22 +0900

    fix

--- mapic/lib/components/inherited-wrapper.dart
+++ mapic/lib/components/inherited-wrapper.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class InheritedWrapper extends InheritedWidget {
   final Function fnc;
-  final int index;
-  InheritedWrapper({required this.fnc, required this.index, required Widget child}) : super(child: child);
+  InheritedWrapper({required this.fnc, required Widget child}) : super(child: child);
 
   static InheritedWrapper of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<InheritedWrapper>() as InheritedWrapper;
 
