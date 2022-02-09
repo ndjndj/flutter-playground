1fb6df95739dc5e8fad2c376ea8a17a7ac1278a9
ndjndj <teq.ndj@gmail.com>
2022-02-09 21:12:15 +0900

    fix

--- mapic/lib/components/inherited-wrapper.dart
+++ mapic/lib/components/inherited-wrapper.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 class InheritedWrapper extends InheritedWidget {
   final Function fnc;
-  InheritedWrapper({required this.fnc, required Widget child}) : super(child: child);
+  final int index;
+  InheritedWrapper({required this.fnc, required this.index, required Widget child}) : super(child: child);
 
   static InheritedWrapper of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<InheritedWrapper>() as InheritedWrapper;
 
   @override
-  bool updateShouldNotify(InheritedWrapper oldWidget) => oldWidget.fnc != fnc;
+  bool updateShouldNotify(InheritedWrapper oldWidget) => oldWidget.index != index;
 }
