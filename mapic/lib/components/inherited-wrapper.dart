02c9f2af04a3ab3741d71a8a28a9d2c77459c2d3
ndjndj <teq.ndj@gmail.com>
2022-02-09 21:11:44 +0900

    ficx

---
+++ mapic/lib/components/inherited-wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InheritedWrapper extends InheritedWidget {
+  final Function fnc;
+  InheritedWrapper({required this.fnc, required Widget child}) : super(child: child);
+
+  static InheritedWrapper of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<InheritedWrapper>() as InheritedWrapper;
+
+  @override
+  bool updateShouldNotify(InheritedWrapper oldWidget) => oldWidget.fnc != fnc;
+}
