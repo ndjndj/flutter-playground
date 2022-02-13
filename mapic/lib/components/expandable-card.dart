697fc7b9d2f62ef6403f3cd12e372e465f7b9c1c
ndjndj <teq.ndj@gmail.com>
2022-02-13 22:15:51 +0900

    f

--- mapic/lib/components/expandable-card.dart
+++ mapic/lib/components/expandable-card.dart
@@ -96,9 +96,9 @@ class _ExpandableCard extends State<ExpandableCard> {
               bottom: 0.0,
               child: Text(
                 "東京都　練馬区　関町一丁目付近",
-                style: TextStyle(
+                style: GoogleFonts.yuseiMagic(
                   fontSize: 18.0,
-                  decoration: TextDecoration.underline
+                  decoration: TextDecoration.underline,
                 ),
               ),
             ),
