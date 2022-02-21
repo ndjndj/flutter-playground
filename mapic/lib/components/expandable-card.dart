0d336d40767bc9d424d3f4c1e5a58075ebf8c659
ndjndj <teq.ndj@gmail.com>
2022-02-21 23:02:13 +0900

    f

--- mapic/lib/components/expandable-card.dart
+++ mapic/lib/components/expandable-card.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
+import '../db/snapshot.dart';
 class ExpandableCard extends StatefulWidget {
   final int id;
   final String place;
