feed9ae0a237ba68497b7e0caa51fdf46098b696
ndjndj <teq.ndj@gmail.com>
2022-02-27 19:15:25 +0900

    f

---
+++ mapic/lib/components/check-dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CheckDialog extends StatefulWidget {
+
+  @override
+  _CheckDialog createState() => _CheckDialog();
+}
+
+class _CheckDialog extends State<CheckDialog>{
+
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
